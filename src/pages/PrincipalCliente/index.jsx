74c8f316d3fa990d85f79efe4ad704e56580955b
Daniel Vieira <124292470+Danzinxit@users.noreply.github.com>
2024-09-07 16:54:49 -0300

    TUDO esta feito 2

--- src/pages/PrincipalCliente/index.jsx
+++ src/pages/PrincipalCliente/index.jsx
@@ -33,15 +33,15 @@ export default function PrincipalCliente() {
     email: "",
   });
 
-  // useEffect(() => {
-  //   if (!state?.token || !state?.id || !state?.nome) {
-  //     navigate("/login");
-  //   } else {
-  //     setToken(state.token);
-  //     setId(state.id);
-  //     setPacienteNome(state.nome);  
-  //   }
-  // }, [navigate, state]);
+  useEffect(() => {
+    if (!state?.token || !state?.id || !state?.nome) {
+    navigate("/login");
+    } else {
+     setToken(state.token);
+     setId(state.id);
+    setPacienteNome(state.nome);  
+    }
+   }, [navigate, state]);
 
   const handleCronometro = () => {
     navigate("/principalCliente/cronometro", { state: { token, id, nome: pacienteNome } });
