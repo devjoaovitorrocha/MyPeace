fb0445c6eaa76dae86626de934a06e7630a1406b
pauloeusantos <paulohenriquedeoliveira2006@gmail.com>
2024-09-29 19:00:23 -0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,6 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<Pagina404 />} />
 
-
         {/* Rotas do Psicólogo */}
         <Route path="/principalPsico" element={<PrincipalPsico />} />
         <Route path="/principalPsico/listapaciente" element={<ListaPaciente />} />
